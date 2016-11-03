c40caf29dae1ba2cd64bf87074a3f22186f93f81
sujit <sujitbehera27@gmail.com>
2016-11-03 17:45:48 -0400

    change

--- WebContent/js/app.js
+++ WebContent/js/app.js
@@ -31,3 +31,10 @@ $(function () {
 $('.header').click(function(){
     $(this).toggleClass('expand').nextUntil('tr.header').slideToggle(200);
 });
+
+$(document).ready(function() {
+
+  // Initialize the plugin
+  $('#my_popup').popup();
+
+});
